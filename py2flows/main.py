d644286b8966d1fa7c999332eccf4267baf76bbf
Layne Liu <layne.liu@outlook.com>
2022-03-09 16:43:14 +0100

    Make it a project

---
+++ py2flows/main.py
@@ -0,0 +1,30 @@
+from py2flows.cfg import comments, flows
+import sys
+import ast
+import logging
+
+print(sys.path)
+
+
+def main():
+    if len(sys.argv) == 1:
+        logging.error('You have to provide a file. One example is py2flows ./examples/test.py')
+        sys.exit(1)
+    file_name = sys.argv[1]
+    file = open(file_name, "r")
+    source = file.read()
+    file.close()
+
+    comments_cleaner = comments.CommentsCleaner(source)
+    comments_cleaner.remove_comments_and_docstrings()
+    comments_cleaner.format_code()
+    logging.debug(comments_cleaner.source)
+
+    cfg = flows.CFGVisitor().build(file_name, ast.parse(comments_cleaner.source))
+    logging.debug('flows: %s', sorted(cfg.flows))
+    logging.debug('edges: %s', sorted(cfg.edges.keys()))
+    cfg.show()
+
+
+if __name__ == '__main__':
+    main()
